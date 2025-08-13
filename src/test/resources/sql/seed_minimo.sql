c600af4c4b1256b3414de2d23f82e7a3ba0cafa0
Santiago3245 <sxarroyo1@espe.edu.ec>
2025-08-13 11:24:47 -0500

    Prueba de CI con H2 para tests

---
+++ src/test/resources/sql/seed_minimo.sql
@@ -0,0 +1,8 @@
+INSERT INTO pacientes (id, nombre, apellido, fecha_nacimiento, email)
+VALUES (1, 'Ana', 'Gómez', '1995-04-10', 'ana@demo.com');
+
+INSERT INTO medicos (id, nombre, apellido, especialidad)
+VALUES (1, 'Jorge', 'Mena', 'Cardiología');
+
+INSERT INTO consultorios (id, numero, piso)
+VALUES (1, 'A-101', 1);
